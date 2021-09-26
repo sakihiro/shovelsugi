8116e80f7c1d69c0f8827e82a5dd2f0edf0fe3ae
EC2 Default User <ec2-user@ip-10-0-1-218.ap-northeast-1.compute.internal>
2021-09-26 06:12:15 +0000

    add cron

--- shovelsugi.py
+++ shovelsugi.py
@@ -37,7 +37,7 @@ client = discord.Client()
 # 文字列の変換
 def convertText(message):
     # wwをわらわらに変換
-    convertText = re.sub('[wWwWｗ]{5,}', 'あっはっは', message)
+    convertText = re.sub('[wWwWｗ]{5,}', 'おおわらわら', message)
     convertText = re.sub('[wWwWｗ]{2,}', 'わらわら', convertText)
     convertText = re.sub('https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', 'URL省略', convertText)
     print({
