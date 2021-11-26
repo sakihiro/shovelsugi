42fdca512f6b08aea441550ee8d6f6f78df6aa40
EC2 Default User <ec2-user@ip-10-0-1-218.ap-northeast-1.compute.internal>
2021-11-26 08:00:50 +0000

    bugfix

--- shovelsugi.py
+++ shovelsugi.py
@@ -11,6 +11,9 @@ import logging
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 from collections import deque
 from botocore.exceptions import ClientError
